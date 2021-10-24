e6215aa24b593e873ef387fcf724cd8a77112966
Pawan Rama Mali <prm@outlook.in>
2021-10-24 17:21:09 +0530

    Added board page for drag and drop items in list

--- srv/board.R
+++ srv/board.R
@@ -1,1 +1,17 @@
-jqui_draggable(ui = "#foo", operation = "enable")
+
+
+output$position <- renderPrint({
+  id <- input$drop_area_dragging
+  top <- input[[paste0(id, '_position')]]$top
+  left <- input[[paste0(id, '_position')]]$left
+  cat('Now dragging: ')
+  cat(sprintf('%s (top: %s, left: %s)', id, top, left))
+  cat('\nDropped: ')
+  cat(input$drop_area_dropped)
+  cat('\nDrop: ')
+  cat(input$drop_area_drop)
+  cat('\nOut: ')
+  cat(input$drop_area_out)
+  cat('\nOver: ')
+  cat(input$drop_area_over)
+})
--- ui/admin/board_page.R
+++ ui/admin/board_page.R
@@ -4,28 +4,148 @@ tabPanel("Board",
          value = "Board",
          fluidPage
          (
+           fluidRow(verbatimTextOutput('position')),
+           
+           
            fluidRow(
-           
-             shinyjqui::jqui_draggable(
-               textInput("bar", "Input")
+             column(
+               width = 3,
+               
+               jqui_draggable(
+                 div(id = 'drg_div1',
+                     'Div',
+                     textInput('drg_input', ''),
+                     selectInput('drg_sel', 'Priority', choices = c("High","Medium","Low")),
+                     textAreaInput('drg_textarea', 'Textarea'),
+                     
+                     style = 'background-color:#79BBF2;
+                     border: 1px dashed;
+                     border-radius: 10px;margin:5px;')
                ),
-             
-             div(
-             jqui_droppable('#foo', options = list(
-               accept = '#bar', # jQuery selector to define which draggable element to monitor. Accept anything if not set.
-               classes = list(
-                 `ui-droppable-active` = 'ui-state-focus', # change class when draggable element is dragging
-                 `ui-droppable-hover` = 'ui-state-highlight' # change class when draggable element is dragging over
+               jqui_draggable(
+                 div(id = 'drg_div2',
+                     'Div',
+                     textInput('drg_input', ''),
+                     selectInput('drg_sel', 'Priority', choices = c("High","Medium","Low")),
+                     textAreaInput('drg_textarea', 'Textarea'),
+                     
+                     style = 'background-color:#79BBF2;
+                     border: 1px dashed;
+                     border-radius: 10px;margin:5px;')
+               )
+               
+               # jqui_draggable(
+               #   div('No id Div', style = 'width:100px; height:100px; background-color:#79BBF2')
+               # ),
+               # 
+               # jqui_draggable(),
+               # jqui_draggable(),
+               # 
+               # 
+               # jqui_draggable()
+               
+             ),
+             column(3,
+             jqui_droppable(
+               div(
+               
+                 style = 'height: 800px; z-index: -10; border: 1px dashed; border-radius: 10px;margin:5px;',
+                 id = 'drop_area1',
+                 'To Do Task List'
                ),
-               drop = JS(
-                 'function(event, ui){$(this).addClass("ui-state-active");}'
-               ) # a javascrip callback to change class when draggable element is dropped in
+               options = list(
+                 drop = JS(
+                   'function(event, ui){',
+                   '  var info = "A " + shinyjqui.getId(ui.draggable) + " is dropped.";',
+                   '  $(this).addClass("ui-state-highlight").html(info);',
+                   '  setTimeout(function(){',
+                   '    $(event.target).removeClass("ui-state-highlight").html("Try to drop something here!");',
+                   '  }, 1500);',
+                   '}'
+                 )
+               )
              )),
-             style= "border:2px;
-                    border-color: black;
-                    border-style: solid;")
+             
+             column(3,
+             jqui_droppable(
+               div(
+                
+                 style = 'height: 800px; z-index: -10; border: 1px dashed; border-radius: 10px;margin:5px;',
+                 id = 'drop_area2',
+                 'In Progress'
+               ),
+               options = list(
+                 drop = JS(
+                   'function(event, ui){',
+                   '  var info = "A " + shinyjqui.getId(ui.draggable) + " is dropped.";',
+                   '  $(this).addClass("ui-state-highlight").html(info);',
+                   '  setTimeout(function(){',
+                   '    $(event.target).removeClass("ui-state-highlight").html("Try to drop something here!");',
+                   '  }, 1500);',
+                   '}'
+                 )
+               )
+             )),
+             column(3,
+             jqui_droppable(
+               div(
+                 
+                 style = 'height: 800px; z-index: -10; border: 1px dashed; border-radius: 10px;margin:5px;',
+                 id = 'drop_area3',
+                 'Completed'
+               ),
+               options = list(
+                 drop = JS(
+                   'function(event, ui){',
+                   '  var info = "A " + shinyjqui.getId(ui.draggable) + " is dropped.";',
+                   '  $(this).addClass("ui-state-highlight").html(info);',
+                   '  setTimeout(function(){',
+                   '    $(event.target).removeClass("ui-state-highlight").html("Try to drop something here!");',
+                   '  }, 1500);',
+                   '}'
+                 )
+               )
+             ))
+           )
+           
+           #   fluidRow(
+           #   column(3,
+           #          jqui_draggable(
+           #            div(id = 'drg_div1',
+           #                'Div',
+           #                textInput('drg_input', 'Input'),
+           #
+           #                style = 'width:100px; height:100px; background-color:#79BBF2')
+           #          ),
+           #          jqui_draggable(
+           #            div(id = 'drg_div2',
+           #                'Div',
+           #                textInput('drg_input', 'Input'),
+           #                style = 'width:100px; height:100px; background-color:#79BBF2')
+           #          )),
+           #
+           #   column(3,
+           #          jqui_droppable(
+           #            column(
+           #              width = 12,
+           #              style = 'height: 800px; z-index: -10; border: 1px dashed; border-radius: 10px;',
+           #              id = 'drop_area',
+           #              'Try to drop something here!'
+           #            ),
+           #            options = list(
+           #              drop = JS(
+           #                'function(event, ui){',
+           #                '  var info = "A " + shinyjqui.getId(ui.draggable) + " is dropped.";',
+           #                '  $(this).addClass("ui-state-highlight").html(info);',
+           #                '  setTimeout(function(){',
+           #                '    $(event.target).removeClass("ui-state-highlight").html("Try to drop something here!");',
+           #                '  }, 1500);',
+           #                '}'
+           #              )
+           #            )
+           #          ))
+           #
+           # )
            
            
-         )
-           
-           ))
+         ))
